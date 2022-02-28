63c9dffc0d0bd23bd8ab46429d3c1ea203ad791b
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-02-28 11:55:51 +0100

    refactor class

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -5,31 +5,12 @@ package lab01.example.model;
  * In particular, a Simple Bank Account allows always the deposit
  * while the withdrawal is allowed only if the balance greater or equal the withdrawal amount
  */
-public class SimpleBankAccountWithAtm implements BankAccountATM {
+public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccountATM {
 
     private static final int FEE = 1;
-    private double balance;
-    private final AccountHolder holder;
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
-        this.holder = holder;
-        this.balance = balance;
-    }
-    @Override
-    public AccountHolder getHolder(){
-        return this.holder;
-    }
-
-    @Override
-    public double getBalance() {
-        return this.balance;
-    }
-
-    @Override
-    public void deposit(final int userID, final double amount) {
-        if (checkUser(userID)) {
-            this.balance += amount;
-        }
+        super(holder, balance);
     }
 
     @Override
@@ -40,24 +21,10 @@ public class SimpleBankAccountWithAtm implements BankAccountATM {
     }
 
     @Override
-    public void withdraw(final int userID, final double amount) {
-        if (checkUser(userID) && isWithdrawAllowed(amount)) {
-            this.balance -= amount;
-        }
-    }
-
-    @Override
     public void withdrawWithATM(final int userID, final double amount) {
         if (checkUser(userID) && isWithdrawAllowed(amount)) {
             this.balance = this.balance - amount - FEE;
         }
     }
 
-    private boolean isWithdrawAllowed(final double amount){
-        return this.balance >= amount;
-    }
-
-    private boolean checkUser(final int id) {
-        return this.holder.getId() == id;
-    }
 }
