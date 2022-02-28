a8a1e88810ba709807f2869fef0eb0ffe578cd56
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2022-02-28 11:41:53 +0100

    refactor test and add basic account with atm

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -1,5 +1,7 @@
 import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * The test suite for testing the SimpleBankAccount with ATM implementation
  */
-class SimpleBankAccountTest extends AbstractSimpleBankAccountTest{
+class SimpleBankAccountWithAtmTest extends AbstractSimpleBankAccountTest{
+
+    private static final int FEE = 1;
 
     @BeforeEach
     void beforeEach(){
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        bankAccount = new SimpleBankAccount(accountHolder, 0);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
     }
-
 }
